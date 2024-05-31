c24968c6b4c2bb68f3b18cd6888f51efeee438ec
leviathan <jake66martin@hotmail.com>
2024-05-31 10:36:34 -0400

    saving changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,13 @@
+// Imports the class objects made within the models
 const User = require('./User')
 const Thread = require('./Thread')
 const Comments = require('./Comments')
+
+
+User.hasMany(Thread, {
+foreignKey: ''
+});
+
+User.hasMany(Comments, {
+foreignKey: 'comm'
+});
